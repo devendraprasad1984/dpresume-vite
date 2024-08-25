020cbb8d9d0718d6f429415ff707de2618ade0dd
DP <devendra.prasad2@bhnetwork.com>
2024-08-25 10:54:32 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import {useState} from "react";
+import "./styles/main.css";
 import "./styles/App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
-  )
+    return <div className="flex col space-between main-outer">
+        <header className="header border">Header</header>
+        <section className="body border flex row gap1 basis space-between">
+            <div className="wid10 border">left</div>
+            <div className="wid100">center</div>
+            <div className="wid20 border">right</div>
+        </section>
+        <footer className="border footer">footer</footer>
+    </div>
 }
 
 export default App
