d448fbedefc19ff4ab9e958271c19515bcb3abaa
DP <devendra.prasad2@bhnetwork.com>
2024-09-22 17:14:22 +0530

    update

--- src/components/body/component/skills.jsx
+++ src/components/body/component/skills.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Skills = () => {
   return <div className="pad10">
-    <div className="size24 base-gimic red-gimic-text">Skills</div>
-    <ul className="double-list rotate20">
+    <div className="size28 base-gimic red-gimic-text">Skills</div>
+    <ul className="double-list rotate20 margin--y-10">
       <li>Javascript</li>
       <li>React.JS</li>
       <li>AEM</li>
