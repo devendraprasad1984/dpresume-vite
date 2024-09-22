ac65ebc35ba16b31b3eef631d7566855e6cd7eb8
DP <devendra.prasad2@bhnetwork.com>
2024-09-22 20:54:38 +0530

    update

--- src/components/body/component/left.jsx
+++ src/components/body/component/left.jsx
@@ -43,6 +43,217 @@ const Left = () => {
       </ul>
     </div>
 
+
+    <div>
+      <h2>Personal Info</h2>
+      <ul className="star-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/">Home</NavLink>
+        </li>
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`} to="/about">About
+            me</NavLink>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h2 className="margin--y-10">Professional Projects</h2>
+      <ul className="star-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/projects">Projects</NavLink>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h2 className="margin--y-10">Out of interest</h2>
+      <ul className="star-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/apps">Apps</NavLink>
+        </li>
+      </ul>
+    </div>
+
+
+    <div>
+      <h2>Personal Info</h2>
+      <ul className="star-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/">Home</NavLink>
+        </li>
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`} to="/about">About
+            me</NavLink>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h2 className="margin--y-10">Professional Projects</h2>
+      <ul className="star-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/projects">Projects</NavLink>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h2 className="margin--y-10">Out of interest</h2>
+      <ul className="star-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/apps">Apps</NavLink>
+        </li>
+      </ul>
+    </div>
+
+
+    <div>
+      <h2>Personal Info</h2>
+      <ul className="star-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/">Home</NavLink>
+        </li>
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`} to="/about">About
+            me</NavLink>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h2 className="margin--y-10">Professional Projects</h2>
+      <ul className="star-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/projects">Projects</NavLink>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h2 className="margin--y-10">Out of interest</h2>
+      <ul className="star-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/apps">Apps</NavLink>
+        </li>
+      </ul>
+    </div>
+
+
+    <div>
+      <h2>Personal Info</h2>
+      <ul className="star-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/">Home</NavLink>
+        </li>
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`} to="/about">About
+            me</NavLink>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h2 className="margin--y-10">Professional Projects</h2>
+      <ul className="star-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/projects">Projects</NavLink>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h2 className="margin--y-10">Out of interest</h2>
+      <ul className="star-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/apps">Apps</NavLink>
+        </li>
+      </ul>
+    </div>
+
+
+    <div>
+      <h2>Personal Info</h2>
+      <ul className="star-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/">Home</NavLink>
+        </li>
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`} to="/about">About
+            me</NavLink>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h2 className="margin--y-10">Professional Projects</h2>
+      <ul className="star-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/projects">Projects</NavLink>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h2 className="margin--y-10">Out of interest</h2>
+      <ul className="star-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/apps">Apps</NavLink>
+        </li>
+      </ul>
+    </div>
+
+
+    <div>
+      <h2>Personal Info</h2>
+      <ul className="star-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/">Home</NavLink>
+        </li>
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`} to="/about">About
+            me</NavLink>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h2 className="margin--y-10">Professional Projects</h2>
+      <ul className="star-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/projects">Projects</NavLink>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h2 className="margin--y-10">Out of interest</h2>
+      <ul className="star-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/apps">Apps</NavLink>
+        </li>
+      </ul>
+    </div>
+
+
   </div>
 }
 export default Left;
