5e1c40ff0ce57433b28f079a384c7519378b0db8
DP <devendra.prasad2@bhnetwork.com>
2024-09-22 17:25:41 +0530

    update

--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -5,7 +5,7 @@ import Right from "./component/right.jsx";
 
 const Body = () => {
     return <React.Fragment>
-        <div className="wid20 main-left mwid100p"><Left/></div>
+        <div className="wid20 main-left mwid100p border-right"><Left/></div>
         <div className="wid100 main-center mwid100p min-height-450 center-route-container overflow"><Center/></div>
         <div className="wid20 main-right mwid100p"><Right/></div>
     </React.Fragment>
--- src/components/body/component/left.jsx
+++ src/components/body/component/left.jsx
@@ -2,22 +2,47 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 
 function seeIfActive() {
-    return ({isActive}) => {
-        return isActive ? "active-link" : "";
-    };
+  return ({isActive}) => {
+    return isActive ? "active-link" : "";
+  };
 }
 
 const Left = () => {
-    return <div className="overflow">
-        <ul className="counter-list">
-            <li className="size20">
-                <NavLink className={`nav-link ${seeIfActive()}`} to="/">Home</NavLink>
-            </li>
-            <li className="size20">
-                <NavLink className={`nav-link ${seeIfActive()}`} to="/about">About
-                    me</NavLink>
-            </li>
-        </ul>
+  return <div className="main-left-container col gap5">
+    <div>
+      <h2>Personal Info</h2>
+      <ul className="double-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/">Home</NavLink>
+        </li>
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`} to="/about">About
+            me</NavLink>
+        </li>
+      </ul>
     </div>
+
+    <div>
+      <h2 className="margin--y-10">Professional Projects</h2>
+      <ul className="double-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/projects">Projects</NavLink>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h2 className="margin--y-10">Out of interest</h2>
+      <ul className="double-list">
+        <li className="size20">
+          <NavLink className={`nav-link ${seeIfActive()}`}
+                   to="/apps">Apps</NavLink>
+        </li>
+      </ul>
+    </div>
+
+  </div>
 }
 export default Left;
