74ce6baf0516573147b6cc186a95e18ef9b7c476
DP <devendra.prasad2@bhnetwork.com>
2025-07-12 21:51:37 +0530

    multi form redux exmaple

--- src/apps/form/submitForm.jsx
+++ src/apps/form/submitForm.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import allSectors from "../redux-app/reducers/allSectors.js";
+import oneliners from "../../../oneliners/oneliners.js";
 
 const SubmitForm = () => {
-
+  const counter = useSelector(allSectors.counterSelector);
+  let formsObject = {};
   const handleSubmit = () => {
-    console.log("form is submitted");
+    for (let i = 0; i < counter; i++) {
+      const thisForm = oneliners.domHelpers.getByDOMId("form-" + i);
+      if (oneliners.core.isObjectPresent(thisForm)) {
+        formsObject["forms-" + i] = {
+          fname: oneliners.domHelpers.getByDOMId("fname-" + i).value,
+          lname: oneliners.domHelpers.getByDOMId("lname-" + i).value,
+          email: oneliners.domHelpers.getByDOMId("email-" + i).value,
+          fname_1: thisForm.childNodes[0].value,
+          lname_1: thisForm.childNodes[1].value,
+          email_1: thisForm.childNodes[2].value
+        };
+      }
+    }
+    console.log("final forms object", formsObject);
   };
 
   return <div className="row gap2">
