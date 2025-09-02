eba81daa8be927c7bd69dd9a9b0aec6389df315a
DP <devendra.prasad2@bhnetwork.com>
2025-09-02 12:42:20 +0530

    MFE-2 module fderation plugin

--- src/microFrontend/ecomm/container/webpack.config.js
+++ src/microFrontend/ecomm/container/webpack.config.js
@@ -1,11 +1,19 @@
-const HtmlWebpackPlugin  = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
+//this becomes client/host of products micro frontend
 module.exports = {
   mode: "development",
   devServer: {
     port: 8080
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        products: "products@http://localhost:8081/remoteEntry.js"
+      }
+    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html"
     })
--- src/microFrontend/ecomm/products/webpack.config.js
+++ src/microFrontend/ecomm/products/webpack.config.js
@@ -1,11 +1,20 @@
-const HtmlWebpackPlugin  = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
+//this becomes server of products micro frontend
 module.exports = {
   mode: "development",
   devServer: {
     port: 8081
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: "products",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./ProductsIndex": "./src/index"
+      }
+    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html"
     })
