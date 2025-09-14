0541d4c3d55bc82bd00fa9982023e634548f954d
DP <devendra.prasad2@bhnetwork.com>
2025-09-14 18:24:47 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,40 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
+
+const filesToExclude = [
+  path.resolve(__dirname, "oneliners"),
+  path.resolve(__dirname, "php-server"),
+  path.resolve(__dirname, "sanskarApp"),
+  path.resolve(__dirname, "mocks"),
+  path.resolve(__dirname, "webapp"),
+  path.resolve(__dirname, "docs")
+];
+
+const createChunks = (id) => {
+  if (id.includes("node_modules")) {
+    return "vendor";
+  }
+  if (id.includes("src/microFrontends")) {
+    return "microFrontends";
+  }
+  // default chunking
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     base: "/",
-    root: "src",
+    root: path.join(__dirname, "src"),
     rollupOptions: {
-      external: [
-        "/SanskarApp/**",
-        "/oneliners/**",
-        "/webapps/**",
-        "/php-server/**",
-        "/__tests__/**"
-      ],
+      external: filesToExclude,
       output: {
         dir: "./dist",
         entryFileNames: "dpresume.js",
         assetFileNames: "dpresume.css",
-        manualChunks: undefined,
+        manualChunks: createChunks,
       }
     },
-    optimizeDeps: {
-      exclude: [
-        "/SanskarApp/**",
-        "/oneliners/**",
-        "/webapps/**",
-        "/php-server/**",
-        "/__tests__/**"
-      ]
-    }
   }
 });
