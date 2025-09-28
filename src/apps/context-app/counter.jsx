42e12676df2213ffd65198a554b50d285b716373
DP <devendra.prasad2@bhnetwork.com>
2025-09-28 20:24:05 +0530

    basic counter

--- src/apps/context-app/counter.jsx
+++ src/apps/context-app/counter.jsx
@@ -6,6 +6,7 @@ const handleTimer = (setCount) => {
   timer = setInterval(() => {
     n += 1;
     if (n > 10) {
+      n = 0;
       clearInterval(timer);
       return;
     }
@@ -15,11 +16,18 @@ const handleTimer = (setCount) => {
 const Counter = () => {
   const [count, setCount] = useState(0);
 
+  const handleReset = () => {
+    n = 0;
+    setCount(0);
+    handleTimer(setCount);
+  };
+
   useEffect(() => handleTimer(setCount), []);
 
-  return <div>
+  return <div class="row gap2 align-center">
     <span>Counter: </span>
-    <span>{count} - {count >= 10 ? "stopped" : ""}</span>
+    <span className="size20 wt600 text-success" style={{minWidth: "50px"}}>{count} {count >= 10 ? "stopped" : ""}</span>
+    <button onClick={handleReset}>reset</button>
   </div>;
 };
 export default Counter;
