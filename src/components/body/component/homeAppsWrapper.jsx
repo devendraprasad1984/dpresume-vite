4077f1649d614711a357574ceb8fdbec5384e44d
dp <devendra.prasad2@bhn.com>
2026-03-10 03:18:53 +0530

    cleanup

--- src/components/body/component/homeAppsWrapper.jsx
+++ src/components/body/component/homeAppsWrapper.jsx
@@ -1,11 +1,6 @@
 import ReduxCounterMain from "../../../apps/redux-app/main.jsx";
-import VanilaJsRedux from "../../../apps/vanila-js-redux/vanila-js-redux.jsx";
 import ContextSampleApp from "../../../apps/context-app/main.jsx";
 import SimpleForm from "../../../apps/simpleForm/simpleForm.jsx";
-import CounterSignal from "../../../otherStateTests/src/counterSignal.jsx";
-import SignalTodos from "../../../otherStateTests/src/signalTodos.jsx";
-import CounterValtio from "../../../otherStateTests/src/valtioCounter.jsx";
-import ValtioTodos from "../../../otherStateTests/src/valtioTodos.jsx";
 
 const HomeAppsWrapper = () => {
   return <div className="col gap2">
@@ -15,20 +10,11 @@ const HomeAppsWrapper = () => {
         <div className="size20 wt600">Sample App - 1</div>
         <div className="col gap2">
           <ContextSampleApp/>
-          <CounterSignal/>
-          <CounterValtio/>
           <ReduxCounterMain/>
         </div>
       </div>
       <div className="size20 wt600">Sample App - 2</div>
       <div className="border overflow pad5"><SimpleForm/></div>
-      <div className="border overflow pad5">
-        <div className="size20 wt600">Sample App - 3</div>
-        <div className="col gap2">
-          <SignalTodos/>
-          <ValtioTodos/>
-        </div>
-      </div>
       <div className="border overflow pad5">
         <img src="/images/mf-1.webp" className="auto-image"/>
       </div>
