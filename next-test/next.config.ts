636108f148b8c9e758077e0a8eda6737f72ff3c7
dp <devendra.prasad2@bhn.com>
2026-06-02 15:04:54 +0530

    next js practice

--- next-test/next.config.ts
+++ next-test/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    turbopack: {
+        // Sets the absolute path to the directory containing next.config.ts
+        root: path.join(__dirname),
+    },
 };
 
 export default nextConfig;
