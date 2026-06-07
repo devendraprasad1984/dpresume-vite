33463e7406292fa9f61aa92ba2ad282c781ad5a8
dp <devendra.prasad2@bhn.com>
2026-06-07 18:28:35 +0530

    mongdb and express and redis practice

--- npm-express-redis/app.js
+++ npm-express-redis/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
-const mongoConStr = "mongodb://localhost:27017";
+const mongoConStr = "mongodb://localhost:27017/testdb";
 // const mongoConStr = "mongodb://localhost:27017/node_cache?authSource=admin"
 const app = express();
 mongoose.connect(mongoConStr);
@@ -13,11 +13,15 @@ const productSchema = new mongoose.Schema({
   specs: Object
 });
 //insert data manually into mongodb and in get it in ProductModel
-const Product = mongoose.model("Product", productSchema);
+const products = mongoose.model("products", productSchema);
 
-app.get("/api/product", async (req, res) => {
-  const products = await Product.find();
-  res.write(products);
+app.get("/api/products", async (req, res) => {
+  const query = {};
+  if (req.query.category) {
+    query.category = req.query.category;
+  }
+  const productData = await products.find(query);
+  res.json(productData);
 });
 
 app.listen(4000, () => console.log("local app server listening on 4000"));
