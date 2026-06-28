9e10419e6bc4a89fcef14a03ca30c885d72622ff
dp <devendra.prasad2@bhn.com>
2026-06-28 21:08:06 +0530

    payload practice

--- payload-cms-test/next.config.ts
+++ payload-cms-test/next.config.ts
@@ -2,9 +2,14 @@ import { withPayload } from '@payloadcms/next/withPayload'
 import type { NextConfig } from 'next'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { redirects } from './redirects'
 
 const __filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(__filename)
+
+const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
 
 const nextConfig: NextConfig = {
   images: {
@@ -12,6 +17,17 @@ const nextConfig: NextConfig = {
       {
         pathname: '/api/media/file/**',
       },
+    ],
+    qualities: [100],
+    remotePatterns: [
+      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+        const url = new URL(item)
+
+        return {
+          hostname: url.hostname,
+          protocol: url.protocol.replace(':', '') as 'http' | 'https',
+        }
+      }),
     ],
   },
   webpack: (webpackConfig) => {
@@ -23,6 +39,7 @@ const nextConfig: NextConfig = {
 
     return webpackConfig
   },
+  redirects,
   turbopack: {
     root: path.resolve(dirname),
   },
